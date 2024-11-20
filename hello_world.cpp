4a9645a657cdde745734fc0f25f9ec7d4bf9bf67
unknown <mikov.ns.04@gmail.com>
2024-11-20 23:42:41 +0300

    Styled with Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-using namespace std;
 
-int main(){
+int 
+main(){
 
 	//комментарий1
 	string name;
-	cin >> name;
+	std::cin >> name;
 	//комментарий2
-	cout << "Hello World, " << name; //комментарий3
+	std::cout << "Hello World, " << name; //комментарий3
 
 	return 0;
 }
